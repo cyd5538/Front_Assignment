175b080eaad6954392020ffc5ac992c25bae7e4d
cyd5538 <cyd5538@naver.com>
2024-07-30 15:41:41 +0900

    Feat: drag condition

--- src/components/DragContext.js
+++ src/components/DragContext.js
@@ -1,12 +1,56 @@
-import React from "react";
+import React, { useState } from "react";
 import { DragDropContext, Droppable, Draggable } from "react-beautiful-dnd";
 import useDrag from "../hook/useDrag";
 
 const DragContext = ({ state, setState }) => {
-  const { onDragEnd } = useDrag(state, setState, () => false);
+  // isInvalid: 현재 드래그가 유효하지 않은지, column: 칼럼 위치
+  const [dragState, setDragState] = useState({ isInvalid: false, column: null });
+
+  const checkMove = (source, destination) => {
+    // 첫 번째 칼럼에서 세 번째 칼럼으로의 이동만 막음
+    if (source.droppableId === 'column-1' && destination.droppableId === 'column-3') {
+      return false;
+    }
+
+    // 소스 아이템과 목적지 아이템을 가져옴 (목적지 아이템이 빈곳이면 null)
+    const sourceItem = state.columns[source.droppableId].items[source.index];
+    const destinationItem = state.columns[destination.droppableId].items[destination.index] || null;;
+
+    // 소스 아이템과 목적지 아이템이 짝수인지 확인
+    const isSourceItem = parseInt(sourceItem.split(" ")[1]) % 2 === 0;
+    const isDestinationItem = destinationItem && parseInt(destinationItem.split(" ")[1]) % 2 === 0;
+
+    // 둘다 짝수이면
+    if (isSourceItem && isDestinationItem) {
+      return false;
+    }
+
+    return true;
+  };
+
+  const { onDragEnd: handleDragEnd } = useDrag(state, setState, checkMove);
+
+  // 드래그가 끝났을 때 실행
+  const onDragEnd = (result) => {
+    handleDragEnd(result); // useDrag hook 실행
+    setDragState({ isInvalid: false, column: null }); // dragState 상태 초기화
+  };
+
+  // 드래그 중에 실행되는 함수
+  const onDragUpdate = (update) => {
+    console.log(update)
+    if (!update.destination) {
+      setDragState({ isInvalid: false, column: null });
+      return;
+    }
+
+    const { source, destination } = update;
+    const isInvalid = !checkMove(source, destination); // 이동이 유효한지 확인
+    setDragState({ isInvalid, column: destination.droppableId });
+  };
 
   return (
-    <DragDropContext onDragEnd={onDragEnd}>
+    <DragDropContext onDragEnd={onDragEnd} onDragUpdate={onDragUpdate}>
       <div className="grid grid-cols-1 xl:grid-cols-4 md:grid-cols-2 gap-4">
         {state.columnOrder.map((columnId) => {
           const column = state.columns[columnId];
@@ -19,7 +63,11 @@ const DragContext = ({ state, setState }) => {
                     ref={provided.innerRef}
                     {...provided.droppableProps}
                     className={`p-2 min-h-[200px] rounded-md ${
-                      snapshot.isDraggingOver ? "bg-sky-300" : "bg-sky-500"
+                      dragState.isInvalid && dragState.column === column.id
+                        ? "bg-red-500"
+                        : snapshot.isDraggingOver 
+                        ? "bg-sky-300"
+                        : "bg-sky-500" 
                     }`}
                   >
                     {column.items.map((item, index) => (
@@ -32,7 +80,7 @@ const DragContext = ({ state, setState }) => {
                             className={`p-4 mb-2 rounded-md ${
                               snapshot.isDragging
                                 ? "bg-sky-500 text-white"
-                                : "bg-white"
+                                : "bg-white" 
                             }`}
                           >
                             {item}
